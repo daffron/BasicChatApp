80076168a05f011488e400b4493c8a0564c5f09a
daffron <daffron92@gmail.com>
2017-06-07 08:18:59 +1200

    Basic chat app working, created git ignore

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # BasicChatApp
+
+A very basic chat app to practice using sockets.io with express
---
+++ index.js
@@ -0,0 +1,17 @@
+let app = require('express')()
+let http = require('http').createServer(app)
+let io = require('socket.io')(http)
+
+app.get('/', (req, res) => {
+   res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', socket => {
+    console.log('a user has connected')
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg)
+    })
+})
+http.listen(3000, () => {
+    console.log('listening on 3000')
+})
